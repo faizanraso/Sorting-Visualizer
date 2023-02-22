35b86248196626a0b2a74b6b54c18abd3ce08cb3
Faizan Rasool <faizanrasool@Faizans-MacBook-Pro.local>
2023-02-22 08:15:22 -0500

    Improved the design of the header

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="flex">
+    <div className="flex mx-auto flex-col items-center justify-center py-2">
       <Header />
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,28 @@
 import React from "react";
+import githubIcon from "../assets/github-mark.svg";
 
 function Header() {
   return (
-    <header className="flex w-screen mt-5 border-b-2 pb-7 sm:px-4 px-2 items-center justify-between">
-      <a className="text-3xl">Algos</a>
+    <header className="flex w-full sm:w-11/12 mt-5 border-b pb-1 sm:px-4 px-2 items-center justify-between font-mono">
+      <a href="/" className="flex text-3xl">
+        <svg fill="currentColor" viewBox="0 0 16 16" height="1em" width="1em">
+          <path d="M12 0H4a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V2a2 2 0 00-2-2zm-2 11.5v-6a.5.5 0 01.5-.5h1a.5.5 0 01.5.5v6a.5.5 0 01-.5.5h-1a.5.5 0 01-.5-.5zm-2.5.5a.5.5 0 01-.5-.5v-4a.5.5 0 01.5-.5h1a.5.5 0 01.5.5v4a.5.5 0 01-.5.5h-1zm-3 0a.5.5 0 01-.5-.5v-2a.5.5 0 01.5-.5h1a.5.5 0 01.5.5v2a.5.5 0 01-.5.5h-1z" />
+        </svg>{" "}
+        <h1 className="px-3 font-bold">Algos</h1>
+      </a>
       <div className="socials">
-        <a href="https://github.com/faizanraso" className="px-2 underline">
-          Github
-        </a>
         <a
-          href="https://www.linkedin.com/in/faizanraso/"
-          className="px-2 underline"
+          href="https://github.com/faizanraso/Sorting-Visualizer"
+          className="px-2 inline"
         >
-          LinkedIn
+          <svg
+            className="hover:fill-white"
+            viewBox="0 0 1024 1024"
+            fill="#6a767a"
+            height="2em"
+          >
+            <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z" />
+          </svg>
         </a>
       </div>
     </header>
